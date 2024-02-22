0fc2778725b836fdbbd5a549050e6af202fee0d8
mariapilan <dudapilan06@hotmail.com>
2024-02-22 14:02:36 -0300

    atualizaçãoat

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+import LoginScreen from "./src/login/LoginScreen";
+
+function App(): JSX.Element {
+  
+  return (
+    <LoginScreen></LoginScreen>
+  );
+}
+
+export default App;
